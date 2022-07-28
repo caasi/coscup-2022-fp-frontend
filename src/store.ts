c0535d86b542770ca6f4e416ca8f6dcd9c6dc234
caasi Huang <caasi.igd@gmail.com>
2022-07-29 06:15:59 +0800

    chore: enable Redux DevTools

--- src/store.ts
+++ src/store.ts
@@ -2,4 +2,13 @@ import { createStore } from 'redux'
 
 import reducer from './reducers'
 
-export default createStore(reducer)
+declare global {
+  interface Window {
+    __REDUX_DEVTOOLS_EXTENSION__?: Function
+  }
+}
+
+export default createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
